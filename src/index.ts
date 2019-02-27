4ec57338508de9debe39dee65fe01517c5f4a373
David Nuño <davidnunogarcia@gmail.com>
2019-02-27 07:37:40 +0100

    ADD env vars. ADD tests

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import * as mqtt from 'mqtt';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const connected = (): void => {
     console.log("Connected to broker");
-    setTimeout(disconnect, 100);
+    setTimeout(disconnect, 1000);
+};
+
+const message = (topic:string, message:string, payload:any): void => {
+    console.log("Message receibed");
 };
 
 const disconnect = (): void => {
@@ -13,11 +20,13 @@ const disconnect = (): void => {
 let client: mqtt.MqttClient;
 
 const initialize = () => {
-    client = mqtt.connect('mqtt://iot.eclipse.org');
+    const endpoint = 'mqtt://'+ (process.env.MQTT_HOST || "iot.eclipse.org")+":"+(process.env.MQTT_PORT || 1883);
+    client = mqtt.connect(endpoint);
     client.on('connect', connected);
+    client.on('message', message);
     client.on('disconnect', disconnect);
 }
 
 initialize();
 
-export { client, connected, disconnect }
+export { initialize, client, connected, disconnect, message }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -13,9 +13,28 @@ describe('Index.js test suite', () => {
         sinon.assert.calledOnce;
     });
 
+    it('should  should be called once', () => {
+        const stub = sinon.stub(index, "message");
+        stub.restore();
+        sinon.assert.notCalled;    
+    });
+
     it('disconnect should be called once', () => {
         const stub = sinon.stub(index, "disconnect");
         stub.restore();
         sinon.assert.calledOnce;       
+    });
+    it('initialize should be called once', () => {
+        const stub = sinon.stub(index, "initialize");
+        stub.restore();
+        sinon.assert.calledOnce;       
+    });
+    it('check process env', () => {
+        try {
+            const host = process.env.MQTT_HOST;
+            chai.expect(host).to.be("string");
+        } catch (error) {
+            
+        }
     });
 });
